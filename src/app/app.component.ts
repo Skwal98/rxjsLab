5c31c6258097c6914a364b040114b3e82e8f7ce1
Skwal98 <levkalex98@gmail.com>
2022-01-20 14:22:08 +0300

    sharing operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,147 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { from, interval, Observable, of, Subject, Subscriber } from 'rxjs';
+import {
+  publish,
+  publishReplay,
+  refCount,
+  share,
+  shareReplay,
+  switchMap,
+  take,
+} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rxjsLab';
+  constructor(httpClient: HttpClient) {
+    /* const ofSub$ = of(1);
+    const one = 1;
+    const testFrom = new Observable((subscriber: Subscriber<number>) => {
+      const array = [1, 2, 3];
+      for (let i = 0; i < array.length && !subscriber.closed; i++) {
+        subscriber.next(array[i]);
+      }
+      subscriber.complete();
+    });
+
+    testFrom.subscribe(
+      () => console.log('of emit'),
+      () => {},
+      () => console.log('of complete')
+    );
+
+    testFrom.subscribe(
+      () => console.log('of emit'),
+      () => {},
+      () => console.log('of complete')
+    );
+
+    const serverRequest$ = httpClient.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    serverRequest$.subscribe(
+      () => console.log('emit'),
+      () => console.log('error'),
+      () => console.log('complete')
+    );
+    serverRequest$.subscribe(
+      () => console.log('emit'),
+      () => console.log('error'),
+      () => console.log('complete')
+    );
+    serverRequest$.subscribe(
+      () => console.log('emit'),
+      () => console.log('error'),
+      () => console.log('complete')
+    );
+
+    const mySubject = interval(1000).pipe(take(2));
+    mySubject.subscribe(
+      () => console.log('my subj'),
+      () => console.log('my subj error'),
+      () => console.log('my subj complete')
+    );
+
+    setTimeout(() => {
+      mySubject.subscribe(
+        () => console.log('my subj'),
+        () => console.log('my subj error'),
+        () => console.log('my subj complete')
+      );
+    }, 5000);*/
+
+    this.data$ = of(1).pipe(
+      switchMap(() =>
+        httpClient.get('https://jsonplaceholder.typicode.com/users')
+      ) /*= this.filter$.pipe(
+      switchMap(() =>
+        httpClient.get('https://jsonplaceholder.typicode.com/users')
+      ),*/
+
+      //share()
+      //если был complete будет подписан заново при помощи нового Subject
+      //актуально для Observable которые при подписке заново эмитят события
+
+      //publish(),
+      //refCount()
+      //если был complete будет вызван только complete
+
+      //принцип работы после срабатывания refCount() (все отписались)
+
+      //shareReplay({ refCount: true, bufferSize: 2 })
+      //shareReplay({ refCount: true, bufferSize: 2 })
+      //если был завершен - выдаст complete, без воспроизведения истории
+      //если не был завершен - подпишется заново, без воспроизведения истории
+
+      //shareReplay({ refCount: false, bufferSize: 1 })
+      //shareReplay({ refCount: false, bufferSize: 1 }) == shareReplay(1)
+      //если был завершен - выдаст историю + complete
+      //если не был завершен - выдаст историю (будет продолжать получать результат, с учетом времени пока не было подписок)
+
+      //publishReplay(1),
+      //refCount()
+      //publishReplay(1),refCount()
+      //если был завершен - выдаст историю + complete
+      //если не был завершен - выдаст историю (будет получать результаты заново)
+    );
+
+    let sub1 = this.data$.subscribe((x) => console.log('sub1'));
+    let sub2 = this.data$.subscribe((x) => console.log('sub2'));
+    this.filter$.next();
+
+    setTimeout(() => {
+      sub1.unsubscribe();
+      sub2.unsubscribe();
+      console.log('all unsubscribe');
+      this.filter$.complete();
+    }, 1000);
+  }
+  filter$ = new Subject<void>();
+  data$?: Observable<object>;
+
+  upd(): void {
+    let sub1 = this.data$!.subscribe(
+      (x) => console.log('[2] sub1'),
+      () => {},
+      () => console.log('[2] sub1 complete')
+    );
+    let sub2 = this.data$!.subscribe(
+      (x) => console.log('[2] sub2'),
+      () => {},
+      () => console.log('[2] sub2 complete')
+    );
+    this.filter$.next();
+  }
+}
+export function fromArrayLike<T>(array: ArrayLike<T>) {
+  return new Observable((subscriber: Subscriber<T>) => {
+    for (let i = 0; i < array.length && !subscriber.closed; i++) {
+      subscriber.next(array[i]);
+    }
+    subscriber.complete();
+  });
 }
